ff6c6b2c8515754fbe120fd93db3fda0610c7fe5
Rafo <rafael_rfs05@hotmail.com>
2023-01-31 19:31:02 -0300

    consertei os 3R's que quebrou e vou tentar faze outro código php para enviar os emails

--- forms/Enviar-Email.php
+++ forms/Enviar-Email.php
@@ -1,34 +1,6 @@
 <?php
-//variaveis
-$name = $_post['name'];
-$email = $_post['email'];
-$phone = $_post['numero'];
-$message = $_post['message'];
-$data_envio = date('d/m/Y');
-$hora_envio = date('H:i:s');
 
-//corpo email
-$arquivo = "
-    <html>
-        <p><b>Nome: </b>$name</p>
-        <p><b>E-mail: </b>$email</p>
-        <p><b>Número: </b>$phone</p>
-        <p><b>Mensagem: </b>$message</p>
-        <p>Este E-mail foi enviado em<b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-";
 
-//Pra quem será enviado
-$destino = "rafael.creeper05@gmail.com";
-$assunto - "Contato pelo site"
+if(isset($_POST))
 
-//Correção
-$headers = "MIME-Version: 1.0\n";
-$headers = "Content-type: text/html; charset=iso-8859-1\n";
-$headers = "Fom: $name <$email>";
-
-//enviar 
-mail($destino, $assunto, $arquivo, $headers);
-
-echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
 ?>
