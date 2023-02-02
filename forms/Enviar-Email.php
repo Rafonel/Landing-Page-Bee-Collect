311b069d5d080b3f554de7a2ce687e87f32f55cc
Rafo <rafael_rfs05@hotmail.com>
2023-02-02 16:52:53 -0300

    Arrumei alguns bugs quando trocava a resolução para a de celular O formulario para enviar e-mail ainda não funciona.. mas irei resolver isso

--- forms/Enviar-Email.php
+++ forms/Enviar-Email.php
@@ -1,6 +1,39 @@
 <?php
 
 
-if(isset($_POST))
+if(isset($_POST(['email']) && !empty($_POST(['email'])))) {
+
+$nome = addslashes($_POST(['name']))
+$email = addslashes($_POST(['email']))
+$numero = addslashes($_POST(['phone']))
+$mensagem = addslashes($_POST(['message']))
+
+
+$to = "rafael.creeper05@gmail.com";
+$subject = "Contato - Coleta";
+$body = "Nome: ".$nome. "\r\n"
+        "Email: ".$email. "\r\n"
+        "Numero: ".$numero. "\r\n"
+        "Assunto: ".$mensagem;
+$header = "From: rafael_rfs05@hotmail.com" . "\r\n"
+           . "Reply to: ".$email. "\e\n"
+           . "X=Mailer:PHP/".phpversion();
+
+if(mail($to,$subject,$body,$header)){
+
+encho("email enviado!");
+
+}
+
+else{
+
+encho("email nao enviado..");
+
+}
+
+
+
+$
+}
 
 ?>
